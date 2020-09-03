75b94c6f1a241d328440be24f013f58b66721c98
Chris <cjcorbin@gmail.com>
2020-09-03 12:48:12 -0700

    All Tests Passed

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, screen, fireEvent, act, getByTestId } from '@testing-library/react';
+import ContactForm from './ContactForm';
+
+test('ContactForm adds new contacts to the list', () => {
+    act(() => {
+    render(<ContactForm />);
+    });
+
+    const firstNameInput = screen.getByTestId(/firstName/i);
+    const lastNameInput = screen.getByTestId(/lastName/i);
+    const emailInput = screen.getByTestId(/email/i);
+    const messageInput = screen.getByTestId(/message/i);
+
+    fireEvent.change(firstNameInput, { target: { value: 'Chris' } });
+    fireEvent.change(lastNameInput, { target: { value: 'Corbin' } });
+    fireEvent.change(emailInput, { target: { value: 'corbin-christopher@lambndastudents.com' } });
+    fireEvent.change(messageInput, { target: { value: 'I may like this better than Cypress!?' } });
+
+    const submitButton = screen.getByTestId(/submit/i);
+
+    act(() => {submitButton.click()
+    });
+
+    expect(firstNameInput).toHaveValue('Chris');
+    expect(lastNameInput).toHaveValue('Corbin');
+    expect(emailInput).toHaveValue('corbin-christopher@lambndastudents.com');
+    expect(messageInput).toHaveValue('I may like this better than Cypress!?');
+    });
