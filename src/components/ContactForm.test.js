3bf5137adf4205d38a8c7372a83d2d88c2f90d0d
Chris <cjcorbin@gmail.com>
2020-09-03 21:43:45 -0700

    Another Approach

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { render, screen, fireEvent, act, getByTestId } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { screen, fireEvent, act } from '@testing-library/react';
 import ContactForm from './ContactForm';
 
+let container;
+
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+});
+
 test('ContactForm adds new contacts to the list', () => {
-    render(<ContactForm />);
+    act(() => {
+        ReactDOM.render(<ContactForm />, container);
+    });
 
     const firstNameInput = screen.getByTestId(/firstName/i);
     const lastNameInput = screen.getByTestId(/lastName/i);
     const emailInput = screen.getByTestId(/email/i);
     const messageInput = screen.getByTestId(/message/i);
 
+    act(() => {
     fireEvent.change(firstNameInput, { target: { value: 'Chris' } });
     fireEvent.change(lastNameInput, { target: { value: 'Corbin' } });
     fireEvent.change(emailInput, { target: { value: 'corbin-christopher@lambndastudents.com' } });
     fireEvent.change(messageInput, { target: { value: 'I may like this better than Cypress!?' } });
+    });
 
     const submitButton = screen.getByTestId(/submit/i);
 
